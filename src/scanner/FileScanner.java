1fdefe8c669eac20dc806a994eb29865321a7ff1
Leonardo Vinicius <leoovncs@gmail.com>
2024-11-19 21:02:25 -0300

    File Scanner

--- src/scanner/FileScanner.java
+++ src/scanner/FileScanner.java
@@ -1,5 +1,7 @@
 package scanner;
 
+import utils.Logger;
+
 import java.io.File;
 
 public class FileScanner {
@@ -9,7 +11,36 @@ public class FileScanner {
         File directory = new File(rootDirectory);
 
         if (!directory.exists()) {
+            Logger.error("The specific repository does not exist: " + rootDirectory);
+            return;
+        }
 
+        if (!directory.isDirectory()){
+            Logger.error("The specified path is not a directory: " + rootDirectory);
+            return;
+        }
+
+        Logger.info("Scanning the directory: " + rootDirectory);
+        scanRecursive(directory);
+    }
+
+    private void scanRecursive(File directory) {
+        File[] files = directory.listFiles();
+
+        if (files == null || files.length == 0) {
+            return;
+        }
+
+        for (File file : files) {
+            if (file.isFile()) {
+                // Check if the file is suspicious
+                if (fileDetector.isTargetFile(file)) {
+                    Logger.warning("File detected: " + file.getAbsolutePath());
+                }
+            } else if (file.isDirectory()) {
+                // Scan subdirectories recursively
+                scanRecursive(file);
+            }
         }
     }
 }
